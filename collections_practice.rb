b5f81f902746be80ff82fe0bf5b430cc74a1faf8
NickisKnowledge <lifechangingstuff@gmail.com>
2019-10-15 18:38:23 -0500

    Start code on lab

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
 
+def sort_array_asc(num_arr)
+  num_arr.sort
+end
+
+
+def sort_array_desc(num_arr)
+  # num_arr.sort.reverse
+  num_arr.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(animal_arr)
+  animal_arr.sort {|x, y| x.length <=> y.length}
+end
